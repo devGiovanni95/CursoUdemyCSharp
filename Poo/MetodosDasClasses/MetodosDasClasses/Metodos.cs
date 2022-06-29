de1a7ed55dfe7b11b9eabaf20df8d1556d8c7f20
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-06-29 20:22:10 -0300

    Passagem de parametros por valor e por referencia

---
+++ Poo/MetodosDasClasses/MetodosDasClasses/Metodos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosDasClasses
+{
+    class Metodos
+    {
+        //Metodos Simples 
+        public void Cumprimentar()
+        {
+            Console.WriteLine("Olá, Seja Bem Vindo");
+        }
+
+        //Metodos com parametros
+        public void Somar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            Console.WriteLine("A soma é: " + resultado);
+        }
+
+        public void Apresentar(string nome, int idade)
+        {
+            Console.WriteLine("Meu nome é " + nome + " e tenho " + idade + " anos");
+        }
+
+        //Passagem de parametros por valor
+        public void AumentarValor(int valor)
+        {
+            valor += 10;
+            Console.WriteLine("Valor final (por valor) é: " + valor);
+        }
+
+        //Passagem de parametros por ref
+        public void AumentarRef(ref int valor)
+        {
+            valor += 10;
+            Console.WriteLine("Valor final (por valor) é: " + valor);
+        }
+    }
+}
--- Poo/MetodosDasClasses/MetodosDasClasses/Program.cs
+++ Poo/MetodosDasClasses/MetodosDasClasses/Program.cs
@@ -10,6 +10,21 @@ namespace MetodosDasClasses
     {
         static void Main(string[] args)
         {
+            Metodos m = new Metodos();
+            m.Cumprimentar();
+            m.Somar(10, 5);
+            m.Apresentar("Giovanni", 26);
+
+            int valor1 = 100;
+            int valor2 = 100;
+
+            m.AumentarValor(valor1);//Nao altera a variavel local
+            m.AumentarRef(ref valor2);//altera a variavel local
+
+            Console.WriteLine("Valor 1: " + valor1);
+            Console.WriteLine("Valor 2: " + valor2);
+
+            Console.ReadKey();
         }
     }
 }
