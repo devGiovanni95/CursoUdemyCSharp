c99c5e326c4421119b6ad6df02b2934ab67adc6b
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-06-29 22:20:50 -0300

    Classes staticas

---
+++ Poo/Estaticas/Estaticas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estaticas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //utilizando a classe statica para passar o parametro por  ->"."<-
+            Matematica.taxa = 10;
+
+            int valor1 = Matematica.Adicionar(200);
+            int valor2 = Matematica.Diminuir(200);
+
+            Console.WriteLine("O valor de 1: " + valor1);
+            Console.WriteLine("O valor de 2: " + valor2);
+
+            Console.ReadKey();
+
+        }
+    }
+}
