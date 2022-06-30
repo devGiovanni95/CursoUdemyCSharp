0354f6ce7c4baa1398537a6dd96c7eb03bdf8ddd
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-06-29 22:04:20 -0300

    Método Construtor

---
+++ Poo/MetodoConstrutor/MetodoConstrutor/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoConstrutor
+{
+    class Pessoa
+    {
+        public string nome;
+        public string sobrenome;
+        public int anoNascimento;
+        public int idade;
+
+        //Definindo valor padrao de inicialização 
+        public Pessoa()
+        {
+            nome = "Desconhecido";
+            sobrenome = "Nenhum";
+            anoNascimento = 0;
+            idade = 0;
+        }
+
+
+     
+    }
+}
