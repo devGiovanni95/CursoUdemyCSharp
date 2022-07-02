2d1653b5717c375b916e569e6381b3eeda37b5c2
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-07-02 20:34:07 -0300

    Propriedades  da classe

---
+++ Poo/Propriedades/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Teste t = new Teste();
+            t.Nome = "Rian";
+            t.Idade = 35;
+            //t.Sobrenome = "dsafvcd";
+
+            Console.WriteLine(t.Nome + " " + t.Sobrenome);
+            Console.WriteLine(t.Idade);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Poo/Propriedades/Teste.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades
+{
+     class Teste
+    {
+        //Definição de um campo
+        private string _nome;
+        public string Sobrenome { get; }= "Almeida ";
+
+        private int _idade;
+
+        //Definião de uma propriedade
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+
+            set
+            {
+                _nome = value;
+            }
+        }
+
+
+        public int Idade
+        {
+            get
+            {
+                return _idade;
+            }
+
+            set
+            {
+                if (value < 18)
+                {
+                    //criando uma regras
+                    Console.WriteLine("Idade não pode ser inferior");
+                }
+                else
+                {
+                    _idade = value;
+                }
+            }
+
+
+        }
+
+     /*   public string Sobrenome()
+        {
+
+        }*/
+
+        public void Apresentar()
+        {
+            if (_nome != "")
+                Console.WriteLine("Bem Vindo " + _nome);
+        }
+
+
+    }
+}
