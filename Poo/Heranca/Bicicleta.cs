0c4564d0f01ce69ff0358605965b1f7f7032b709
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-07-03 23:41:36 -0300

    Herança, criando classes para criar uma conexão com herança

---
+++ Poo/Heranca/Bicicleta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    internal class Bicicleta
+    {
+        public string Cor { get; set; }
+        public string Marca { get; set; }
+
+
+        public void Pedalar()
+        {
+            Console.WriteLine("Pedalou!");
+        }
+
+        public void Acelerar()
+        {
+            Console.WriteLine("Acelerou!");
+        }
+
+        public void Parar()
+        {
+            Console.WriteLine("Parou!");
+        }
+    }
+}
