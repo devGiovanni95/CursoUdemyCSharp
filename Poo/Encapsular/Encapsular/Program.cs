554b28e447078186c4fdad242dea135b8f99921a
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-07-03 23:02:00 -0300

    Encapsulamento Inicio

---
+++ Poo/Encapsular/Encapsular/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c = new Conta();
+
+            c._cliente = "Gabriel";
+            c._saldo = 100;
+
+            //==================Operação de Deposito========================
+            double valor = 100;
+            c.Depositar(valor);
+
+            //=====================Operação de Saque========================
+            double valor1 = 50;
+            c.Sacar(valor1);
+
+            //==================Resultado do Saldo Atual========================
+            Console.WriteLine("Cliente: " + c._cliente);
+            Console.WriteLine("Saldo: " + c._saldo); ;
+
+            Console.ReadKey ();
+        }
+    }
+}
