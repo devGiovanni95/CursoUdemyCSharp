eeabd7432e83c94a69e9f56c669e060eff5ff3ed
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-07-05 20:44:17 -0300

    Modificadores de Acesso

---
+++ Poo/Modificadores/Classes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificadores
+{
+     class Teste
+    {
+        private string nome;
+        public string sobrenome;
+
+        private void Metodo1() { }
+
+        public void Executar() { }
+    }
+
+    class Humano
+    {
+        //Fica invisivel para as outras classes
+        //Mas pra qualquer classe que herda dela ficam disponivel
+        protected string nome;
+
+        //Fica invisivel para todas as outras classes
+        private string sobrenome;
+        
+        //so pode ser usado na aplicação em que é implementada
+        //se outra aplicação fosse usar dados desta aplicação
+        //este campo nao ficaria disponivel para acesso 
+        internal int idade;
+    }
+
+    class Homem : Humano
+    {
+        public void Metodo()
+        {
+            nome = "h";
+            
+        }
+    }
+}
---
+++ Poo/Modificadores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste t = new Teste();
+           // t.nome = "";
+            t.sobrenome = "Eminem";
+           //t.Metodo1();
+            t.Executar();
+
+            Homem h = new Homem();
+            h.idade = 18;
+            Console.WriteLine(h+"\n"+t.sobrenome);
+
+            //Como definimos nome privado ele e restrito
+            //h.nome = "Eli"; 
+
+            //Como definimos nome protected ele e restrito
+            //h.sobrenome = "Soares"; 
+
+            Console.ReadKey();
+
+        }
+    }
+}
