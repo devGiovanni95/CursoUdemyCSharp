21a780bcf1b077337aa76fc9f9bfa75d81665ef2
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-07-05 22:29:02 -0300

    Trabalhando com Polimorfismo

---
+++ Poo/Polimorfismo/Classes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public class Forma
+    {
+        public int x { get; private set; }
+        public int y { get; private set; }
+        public int Altura { get;  set; }
+        public int Largura { get;  set; }
+        public int Raio { get;  set; }
+
+        public virtual void Desenhar()
+        {
+            Console.WriteLine("Preparando-se para desenhar");
+        }
+        
+        public virtual void Area()
+        {
+           // Console.WriteLine("Preparando-se para desenhar");
+        }
+
+    }
+
+    public class Circulo : Forma
+    {
+        public override void Desenhar()
+        {
+            //pegando as implementações da Classe base para reaproveitar os codigos
+            base.Desenhar();
+            Console.WriteLine("Desenhando um Circulo");
+        }
+
+        public override void Area()
+        {
+            double area = 3.14 * (Raio * Raio);
+            Console.WriteLine("A area do Circulo " + area);
+        }
+    }
+
+    public class Retangulo : Forma
+    {
+        public override void Desenhar()
+        {
+            //Code to draw a Retangulo
+            base.Desenhar();
+            Console.WriteLine("Desenhando um Retangulo");
+        }
+
+        public override void Area()
+        {
+            double area = Largura * Altura;
+            Console.WriteLine("A area do Retangulo " + area);
+        }
+    }
+
+    public class Triangulo : Forma
+    {
+        public override void Desenhar()
+        {
+            //Code to draw a Triangulo
+            Console.WriteLine("Desenhando um Triangulo");
+        }
+
+        public override void Area()
+        {
+            double area = (Largura * Altura)/2;
+            Console.WriteLine("A area do Triangulo " + area);
+        }
+
+    }
+
+}
---
+++ Poo/Polimorfismo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Polimorfismo sao todas do mesmo tipo mais tem
+            //comportamentos diferentes com implementações
+            //especificas na sua implementação 
+            //se a classe fosse ndo tipo abstrata
+            //so poderiamos instanciar as com os tipos predefinidos nao imprimindo a base
+            Forma a = new Forma();
+            a.Altura = 10;
+            a.Largura = 10;
+            a.Raio = 10;
+            Forma b = new Triangulo();
+            b.Altura = 10;
+            b.Largura = 10;
+            Forma c = new Circulo();
+            c.Raio = 10;
+            Forma d = new Retangulo();
+            a.Altura = 10;
+            a.Largura = 10;
+
+            Console.WriteLine("Forma");
+            a.Desenhar();
+            a.Area();
+            Console.WriteLine("Triangulo");
+            b.Desenhar();
+            b.Area();
+            Console.WriteLine("Circulo");            
+            c.Desenhar();
+            c.Area();
+            Console.WriteLine("Retangulo");
+            d.Desenhar();
+            d.Area();
+
+            Console.ReadKey();
+        }
+    }
+}
